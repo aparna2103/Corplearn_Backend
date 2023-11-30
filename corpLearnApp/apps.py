133133fa81d086f703b90ef502f1a9e8308f0b7d
= <vnutalap@umd.edu>
2023-11-30 15:56:37 -0500

    dd

--- corpLearnApp/apps.py
+++ corpLearnApp/apps.py
@@ -7,5 +7,5 @@ class CorplearnappConfig(AppConfig):
 
     def ready(self):
         from .services import UserService
-        UserService.add_default_role()
-        UserService.add_test_admin_user()
+        # UserService.add_default_role()
+        # UserService.add_test_admin_user()
