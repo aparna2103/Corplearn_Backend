532a945591f70650c7a17e03b2a9822bc491eb77
= <vnutalap@umd.edu>
2023-11-30 15:33:33 -0500

    dd

--- run_app.sh
+++ run_app.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Define the directory for the virtual environment
-VENV_DIR="myenv"
+# VENV_DIR="myenv"
 
-# Check if the virtual environment directory exists, if not, create one
-if [ ! -d "$VENV_DIR" ]; then
-    python3 -m venv $VENV_DIR
-    echo "Virtual environment created."
-fi
+# # Check if the virtual environment directory exists, if not, create one
+# if [ ! -d "$VENV_DIR" ]; then
+#     python3 -m venv $VENV_DIR
+#     echo "Virtual environment created."
+# fi
 
-# Activate the virtual environment
-source $VENV_DIR/bin/activate
+# # Activate the virtual environment
+# source $VENV_DIR/bin/activate
 
-# Install dependencies from requirements.txt
-pip install -r requirements.txt
+# # Install dependencies from requirements.txt
+# pip install -r requirements.txt
 
 # Apply migrations
 python manage.py migrate
